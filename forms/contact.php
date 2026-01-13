ecb76b1e95ea5e7c913328ac1a1388d398ed50d1
RioIsnan <rio.rigr9@gmail.com>
2026-01-14 03:48:16 +0700

    update portofolio

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,41 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+require '../assets/vendor/phpmailer/Exception.php';
+require '../assets/vendor/phpmailer/PHPMailer.php';
+require '../assets/vendor/phpmailer/SMTP.php';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$mail = new PHPMailer(true);
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+try {
+    // CONFIG SMTP
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'rio.rigr9@gmail.com';   // EMAIL KAMU
+    $mail->Password   = 'APP_PASSWORD_GMAIL';    // NANTI DIISI
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+    // SENDER
+    $mail->setFrom($_POST['email'], $_POST['name']);
+    $mail->addAddress('rio.rigr9@gmail.com'); // Email tujuan (kamu)
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+    // ISI EMAIL
+    $mail->isHTML(true);
+    $mail->Subject = $_POST['subject'];
+    $mail->Body    = "
+        <h3>Pesan Baru dari Website CV</h3>
+        <p><b>Nama:</b> {$_POST['name']}</p>
+        <p><b>Email:</b> {$_POST['email']}</p>
+        <p><b>Pesan:</b><br>{$_POST['message']}</p>
+    ";
 
-  echo $contact->send();
+    $mail->send();
+    echo 'OK';
+
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 ?>
